ecf5ac7ed3adcf194d1a3c773d4f61eae1a3b754
Max Smith <max.smith.2425@gmail.com>
2023-11-03 20:10:03 -0400

    added routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Dash from "../src/Dashboard.jsx";
+import Signin from "../src/auth/SignIn.jsx";
+import "./index.css";
+import { BrowserRouter,Route, Routes} from "react-router-dom";
+import ForgotPassword from "../src/auth/ForgotPassword.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+  
+    <Routes >
+      <Route path="/" element={<Signin/>} />
+       <Route path="/dash" element={<Dash/>} />
+       <Route path = "/forgotPassword" element= {<ForgotPassword/>}/>
+      </Routes>
+  </BrowserRouter>,
+);
