25fe99b5526621736cda61155d9862a47293cab4
Collin Myers <collin_myers@outlook.com>
2023-11-07 13:59:18 -0500

    Changed folder structure and styling

---
+++ src/Pages/auth/SignIn.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Card, TextField, Button } from "@mui/material";
+import { Account, Client } from "appwrite";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import {validateEmail} from "../../utils/Validators";
+import { useNavigate } from "react-router-dom";
+import "../../styling/AuthStyle.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+
+LoginScreen.propTypes = {
+  handleLoginSuccess: PropTypes.func.isRequired,
+};
+
+
+export default function LoginScreen({ handleLoginSuccess }) {
+
+  const InvalidCreds = () => {
+    toast.error("Invalid Username or Password", {
+        position: toast.POSITION.TOP_CENTER
+    });
+};
+
+  const ValidCreds = () => {
+    toast.success("Successfuly Signed in!",{
+        position: toast.POSITION.TOP_CENTER
+    });
+  };
+
+
+  const navigate = useNavigate();
+
+    const navigateToDash = () => {
+      navigate("/dash");
+    };
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+
+
+  const handleLogin = async () => {
+    try {
+      const client = new Client()
+        .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // this is not working
+         .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
+
+      const account = new Account(client);
+      await account.createEmailSession(email, password);
+
+      setEmail("");
+      setPassword("");
+      ValidCreds();
+      navigateToDash();
+      // Navigation can be handled using React Router's Link component.
+    } catch (error) {
+      InvalidCreds();
+      console.error(error);
+
+    }
+  };
+
+  return (
+    <div className="container">
+      <ToastContainer/>
+      <div className="center">
+      <h2 className="AuthTitle">Sign In</h2>
+          <TextField
+            className="EmailTextfield"
+            label="Email"
+            fullWidth
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            onBlur={() => validateEmail(email, setEmail)}
+          />
+          <TextField
+            className="PasswordText"
+            label="Password"
+            fullWidth
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleLogin}
+          >
+            Sign In
+          </Button>
+          <Link className="ForgotPassLink" to="/forgotPassword">Forgot Password</Link>
+          </div>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Dash from "../src/Dashboard.jsx";
-import Signin from "../src/auth/SignIn.jsx";
 import "./index.css";
-import { BrowserRouter,Route, Routes} from "react-router-dom";
-import ForgotPassword from "../src/auth/ForgotPassword.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ForgotPassword from "./Pages/auth/ForgotPassword";
+import Signin from "./Pages/auth/SignIn";
+import Dash from "./Pages/dashboard/Dash";   
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-  
-    <Routes >
-      <Route path="/" element={<Signin/>} />
-       <Route path="/dash" element={<Dash/>} />
-       <Route path = "/forgotPassword" element= {<ForgotPassword/>}/>
-      </Routes>
-  </BrowserRouter>,
+    <BrowserRouter>
+
+        <Routes >
+            <Route path="/" element={<Signin />} />
+            <Route path="/dash" element={<Dash />} />
+            <Route path="/forgotPassword" element={<ForgotPassword />} />
+        </Routes>
+    </BrowserRouter>,
 );
