9558d60aa517a7205b9a1be6441ff11bacdaa45d
Max Smith <max.smith.2425@gmail.com>
2023-11-20 16:17:54 -0500

    acount status verification

--- src/Pages/auth/SignIn.jsx
+++ src/Pages/auth/SignIn.jsx
@@ -1,51 +1,69 @@
-import React, { useState } from "react";
-import { Card, TextField, Button } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import { TextField, Button } from "@mui/material";
 import { Account, Client } from "appwrite";
-import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import {validateEmail} from "../../utils/Validators";
 import { useNavigate } from "react-router-dom";
-import "../../styling/AuthStyle.css";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import "../../styling/AuthStyle.css";
 
 
-LoginScreen.propTypes = {
-  handleLoginSuccess: PropTypes.func.isRequired,
-};
-
-
-export default function LoginScreen({ handleLoginSuccess }) {
+export default function LoginScreen() {
+  const navigate = useNavigate();
 
   const InvalidCreds = () => {
     toast.error("Invalid Username or Password", {
-        position: toast.POSITION.TOP_CENTER
-    });
-};
-
-  const ValidCreds = () => {
-    toast.success("Successfuly Signed in!",{
-        position: toast.POSITION.TOP_CENTER
+      position: toast.POSITION.TOP_CENTER,
     });
   };
 
+  const ValidCreds = () => {
+    toast.success("Successfully Signed in!", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
 
-  const navigate = useNavigate();
+  const navigateToDash = () => {
+    navigate("/dash");
+  };
 
-    const navigateToDash = () => {
-      navigate("/dash");
-    };
+  // const navigateToNewEvent = () => {
+  //   navigate("/newEvent");
+  // };
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    // Check if the user is already logged in
+    const checkLoggedInStatus = async () => {
+      try {
+        const client = new Client()
+          .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+          .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
 
+        const account = new Account(client);
+
+        // Check if the user is already logged in
+        if (await account.get()) {
+          setIsLoggedIn(true);
+          // Redirect to the dashboard or another protected route
+          navigateToDash();
+        }
+      } catch (error) {
+        console.log("not Logged in");
+      }
+    };
+
+    checkLoggedInStatus();
+  }, []); // Empty dependency array ensures that this effect runs only once on component mount
 
   const handleLogin = async () => {
     try {
       const client = new Client()
-        .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT) // this is not working
-         .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
+        .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+        .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
 
       const account = new Account(client);
       await account.createEmailSession(email, password);
@@ -54,44 +72,45 @@ export default function LoginScreen({ handleLoginSuccess }) {
       setPassword("");
       ValidCreds();
       navigateToDash();
-      // Navigation can be handled using React Router's Link component.
+      
     } catch (error) {
       InvalidCreds();
       console.error(error);
-
     }
   };
 
+  if (isLoggedIn) {
+    // Redirect the user to the dashboard if already logged in
+    return null;
+  }
+
   return (
     <div className="container">
-      <ToastContainer/>
+      <ToastContainer />
       <div className="center">
-      <h2 className="AuthTitle">Sign In</h2>
-          <TextField
-            className="EmailTextfield"
-            label="Email"
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            onBlur={() => validateEmail(email, setEmail)}
-          />
-          <TextField
-            className="PasswordText"
-            label="Password"
-            fullWidth
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleLogin}
-          >
-            Sign In
-          </Button>
-          <Link className="ForgotPassLink" to="/forgotPassword">Forgot Password</Link>
-          </div>
+        <h2 className="AuthTitle">Sign In</h2>
+        <TextField
+          className="EmailTextfield"
+          label="Email"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          className="PasswordText"
+          label="Password"
+          fullWidth
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" color="primary" onClick={handleLogin}>
+          Sign In
+        </Button>
+        <Link className="ForgotPassLink" to="/forgotPassword">
+          Forgot Password
+        </Link>
+      </div>
     </div>
   );
 }
