dcc605f6d76bdeb55201305d9066c8f9fe4f0a00
Max Smith <max.smith.2425@gmail.com>
2023-11-20 16:17:16 -0500

    new route

--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
+
 import ForgotPassword from "./Pages/auth/ForgotPassword";
 import Signin from "./Pages/auth/SignIn";
 import Dash from "./Pages/dashboard/Dash";   
+import NewEvent from "./Pages/dashboard/NewEvent";
 
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
-
         <Routes >
             <Route path="/" element={<Signin />} />
             <Route path="/dash" element={<Dash />} />
             <Route path="/forgotPassword" element={<ForgotPassword />} />
+            <Route path= "/newEvent" element ={<NewEvent/>}/>
         </Routes>
     </BrowserRouter>,
 );
