ede2c6bedb5df96fd2465d672654f7bad043f4ec
Max Smith <max.smith.2425@gmail.com>
2023-11-21 17:00:09 -0500

    edit event template

---
+++ src/Pages/dashboard/EditEvent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../styling/EditEventStyle.css";
+
+
+
+
+export default function EditEvent(){
+
+  return (
+    <div>
+        <div className="editEventContainer">
+            <h1 className="title">Edit Event</h1>
+        </div>
+    </div>
+  );
+}
+
