c311268ccb153a711cdf59f6be359a10f725d5f8
mas8190 <mas8190>
2024-02-05 21:13:49 -0500

    replace realtime delete event

--- src/Pages/dashboard/EventComponents/DeleteEvent.jsx
+++ src/Pages/dashboard/EventComponents/DeleteEvent.jsx
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from "react";
 import "../../../styling/DeleteEventStyle.css";
-import { Databases, Client } from "appwrite";
+import { Databases, Client, Storage } from "appwrite";
 import { toast,ToastContainer } from "react-toastify";
 
 
-export default function DeleteEvent() {
+export default function DeleteEvent({onDataChange}) {
   const [list, setList] = useState([]);
   const [selectedItem, setSelectedItem] = useState(null);
   const [dropdownVisible, setDropdownVisible] = useState(false);
-
+  const [successfullDeletion, setSuccessfullDeletion] = useState(false);
+  
+  const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
+ 
   const toggleDropdown = () => {
     setDropdownVisible(!dropdownVisible);
   };
@@ -30,27 +35,19 @@ export default function DeleteEvent() {
   
 
   useEffect(() => {
+    console.log("mount");
+  if(successfullDeletion){
+    onDataChange();
+    setSuccessfullDeletion(false);
+  }
   
-    const client = new Client()
-      .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-      .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
-    const handleSubscription = () =>{
-      getEvents();
-    }
-
-    client.subscribe(
-      "databases.653ae4b2740b9f0a5139.collections.655280f07e30eb37c8e8.documents",
-      handleSubscription
-  );
   getEvents();
 
-  }, []);
+  
+  }, [onDataChange,successfullDeletion]);
 
   const getEvents = async () => {
 
-    const client = new Client()
-    .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-    .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
 
   // Initialize the Databases class
   const database = new Databases(client);
@@ -66,26 +63,29 @@ export default function DeleteEvent() {
     .catch((error) => {
       console.error("Error fetching documents:", error);
     });
-  }
+  };
 
   const  handleSelectEvent = async () =>{
     if (selectedItem) {
         try {
-          const client = new Client()
-            .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-            .setProject(import.meta.env.VITE_APPWRITE_PROJECT);
-  
+        
           const database = new Databases(client);
           const databaseID = "653ae4b2740b9f0a5139";
           const collectionId = "655280f07e30eb37c8e8";
-  
+          // const storage = new Storage(client);
+
+
           await database.deleteDocument(databaseID, collectionId, selectedItem.$id);
+
           // Optional: You can update the list to reflect the changes after deletion
           // Clear the selected item
           setSelectedItem(null);
+          setSuccessfullDeletion(true);
+          deleteImage();
           SuccessfullDeletion(); //success pop up
           
         } catch (error) {
+          setSuccessfullDeletion(false);
           DeletionFailed();
           console.error("Error deleting document:", error);
         }
@@ -94,6 +94,17 @@ export default function DeleteEvent() {
 
       }
   }; 
+
+  const deleteImage = async () =>{
+    const storage = new Storage(client);
+    const promise = storage.deleteFile("653ae4d2b3fcc68c10bf",selectedItem.FileID);
+
+    promise.then(function (response) {
+      console.log(response); // Success
+  }, function (error) {
+      console.log(error); // Failure
+  }); 
+  };
 
   
 
@@ -116,11 +127,7 @@ export default function DeleteEvent() {
         <button className="dropdown-toggle" onClick={toggleDropdown}>
           {selectedItem ? selectedItem.Name : "Select Event"}
         </button>
-        {dropdownVisible && (
-          <div className="dropdown-wrapper">
-            {list.map((item) => renderList({ item }))}
-          </div>
-        )}
+        {dropdownVisible && ( <div className="dropdown-wrapper">{list.map((item) => renderList({ item }))} </div>)}
         <button className="submit" onClick={handleSelectEvent}>Delete Event</button>
       </div>
       
