3e7489f5330ce75d998ec7a9506bbd79b28d785e
mas8190 <mas8190>
2024-02-13 16:28:44 -0500

    delete poi

--- src/Pages/dashboard/POIComponents/DeletePOI.jsx
+++ src/Pages/dashboard/POIComponents/DeletePOI.jsx
@@ -1,27 +1,95 @@
 import React, { useState, useEffect } from "react";
-import { Databases, Client,ID, Account, Storage} from "appwrite";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "../../../styling/DeletePOIStyle.css";
 import { toast,ToastContainer } from "react-toastify";
-
+import {database} from "../../../utils/AppwriteConfig";
+import {MAP_COLLECTION_ID} from "../../../utils/AppwriteConfig";
+import {DATABASE_ID} from "../../../utils/AppwriteConfig";
 
 export default function DeletePOI(){
+    const [list,setList] = useState([]);
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [selectedPOI, setSelectedPOI] = useState("");
+  
+ 
+    const toggleDropdown = () => {
+      setDropdownVisible(!dropdownVisible);
+    };
 
+    const SuccessfullDeletion = () => {
+        toast.success("Event Deleted", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      
+      };
+      
+      const DeletionFailed = () => {
+        toast.error("Failed to Delete Event", {
+          position: toast.POSITION.TOP_CENTER,
+        });
+      };
+
+    useEffect(() =>{
+        getPOI();
+    },[]);
+
+    const getPOI = async () => {
+
+        database
+          .listDocuments(DATABASE_ID, MAP_COLLECTION_ID)
+          .then((response) => {
+            console.log("Documents:", response.documents);
+            setList(response.documents);
+          })
+          .catch((error) => {
+            console.error("Error fetching documents:", error);
+          });
+        };
+
+    const renderList = ({ item }) => {
+        return (
+            <div key={item.$id} className="dropdown-item" >
+            <p style={{ color: selectedPOI === item ? "#8FA063" : "#134C77" }} onClick={() => {
+                setSelectedPOI(item);
+                setDropdownVisible(false);
+                }}>{item.Name}</p>
+            </div>
+            );
+    };
+        
+    const  handleSelectEvent = async () =>{
+            if (selectedPOI) {
+                try {
+                
+                  
+                  await database.deleteDocument(DATABASE_ID, MAP_COLLECTION_ID, selectedPOI.$id);
+                 setSelectedPOI(null);
+                 SuccessfullDeletion();
+                  
+                } catch (error) {
+                  
+                  console.error("Error deleting document:", error);
+                }
+              }else{
+                DeletionFailed();
+        
+              }
+          };    
+
+    
 
 
 
     return(
         <div className="deletePOIContainer">
         <h1 className="title">Delete POI</h1>
-        <input className="uploader" type="file"  id="uploader" />
-        <input className="eventName" type="text" placeholder="Name"  />
-        <input className= "eventDate" type="text" placeholder="Date"  />
-        <input type="text" placeholder="Short Description"  />
-        <input type="text" placeholder="Long Description" />
-        <input type="number" placeholder="Latitude"  />
-        <input type="number" placeholder="Longitude"  />
-        <button >Delete POI</button>
+        <button className="dropdown-toggle" onClick={toggleDropdown}>
+          {selectedPOI ? selectedPOI.Name : "Select Event"}
+        </button>
+        {dropdownVisible && ( <div className="dropdown-wrapper">{list.map((item) => renderList({ item }))} </div>)}
+
+        <button onClick={handleSelectEvent}>Delete POI</button>
         </div>
 
     );
