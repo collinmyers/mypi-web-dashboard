51f7c2e2deefd6585b12f121c1c3e29f482c3854
mas8190 <mas8190>
2024-02-13 16:28:36 -0500

    create poi + style

--- src/Pages/dashboard/POIComponents/CreatePOI.jsx
+++ src/Pages/dashboard/POIComponents/CreatePOI.jsx
@@ -1,34 +1,70 @@
 import React, { useState, useEffect } from "react";
-import { Databases, Client,ID, Account, Storage} from "appwrite";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import "../../../styling/CreatePOIStyle.css";
-import { toast,ToastContainer } from "react-toastify";
+import {ID} from "appwrite";
+import {account} from "../../../utils/AppwriteConfig";
+import {database} from "../../../utils/AppwriteConfig";
+import {storage} from "../../../utils/AppwriteConfig";
+import {BUCKET_ID} from "../../../utils/AppwriteConfig";
+import {MAP_COLLECTION_ID} from "../../../utils/AppwriteConfig";
+import {DATABASE_ID} from "../../../utils/AppwriteConfig";
 
 
 export default function CreatePOI(){
 
+const [name,setName] = useState("");
+const [latitude,setLatitude] = useState("");
+const [longitude,setLongitude] = useState("");
+const [status, setStatus] = useState("Open");
+const [type, setType] = useState("");
 
+useEffect(()=>{
 
+});
 
+const handleSubmit = async () => {
 
+    console.log(name);
+    console.log(latitude);
+    console.log(longitude);
+    console.log(status);
+    console.log(type);
 
+   const data = {
+    Name: name,
+    Latitude: latitude,
+    Longitude: longitude,
+    Status: status,
+    Type: type
+   };
 
-
+   try{
+    const response = await database.createDocument(DATABASE_ID,MAP_COLLECTION_ID, ID.unique(), data);
     
+   }catch(response){
+    console.log(response);
+   }
+
+};
 
     return(
         <div className="createPOIContainer">
-        
         <h1 className="title">New POI</h1>
-        <input className="uploader" type="file"  id="uploader" />
-        <input className="eventName" type="text" placeholder="Name"  />
-        <input className= "eventDate" type="text" placeholder="Date"  />
-        <input type="text" placeholder="Short Description"  />
-        <input type="text" placeholder="Long Description" />
-        <input type="number" placeholder="Latitude"  />
-        <input type="number" placeholder="Longitude"  />
-        <button >Create POI</button>
+        <input className="poiName" type="text" placeholder="Name" onChange={(e) => setName(e.target.value)}  />
+        <input type="number" placeholder="Latitude" onChange={(e) => setLatitude(e.target.value)} />
+        <input type="number" placeholder="Longitude" onChange={(e) => setLongitude(e.target.value)}/>
+        <label className="statusLabel">
+        Status:      
+        <select name="poiStatus" value={status} onChange={(e) => setStatus(e.target.value)}>
+          <option value="Open">Open</option>
+          <option value="Closed">Closed</option>
+        </select>
+        </label>
+        <input className="poiType" type="text" placeholder="Type" onChange={(e) => setType(e.target.value)} />
+
+        
+        <button onClick={handleSubmit}>Create POI</button>
         </div>
 
     );
