f09cef9e4712ef02e08c2c93932c6f16aec7846a
mas8190 <mas8190>
2024-02-15 15:47:52 -0500

    Events edit table functionality and styling

---
+++ src/Pages/dashboard/EventComponents/EventsTable.jsx
@@ -0,0 +1,96 @@
+import {React,useState} from "react";
+import "../../../styling/EventsStyling/EventsTableStyle.css";
+
+
+
+const EventsTable = ({ data,deleteEvent,editEvent,createEvent}) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageSize] = useState(5);
+
+  const handleSearchChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    setSearchTerm(value);
+    setCurrentPage(1); // Reset to first page when searching
+  };
+
+  const handleDeleteClick = (item) => {
+    const isConfirmed = window.confirm("Are you sure you want to delete?");
+    if (isConfirmed) {
+      deleteEvent(item);
+    }
+  };
+
+  const filteredData = data.filter((item) =>
+    item.Name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const totalPages = Math.ceil(filteredData.length / pageSize);
+  const startIndex = (currentPage - 1) * pageSize;
+  const currentPageData = filteredData.slice(startIndex, startIndex + pageSize);
+
+  const fetchPreviousPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  };
+
+  const fetchNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
+  };
+
+  return (
+    <div>
+      <div className="search-container">
+        <input type="text" placeholder="Search by name..." value={searchTerm} onChange={handleSearchChange} />
+      </div>
+      <div className="table-container">
+        <div className="create-event-container">
+          <button onClick={() => createEvent()} className="create-event-button">Create Event</button>
+        </div>
+        <table className="custom-table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Latitude</th>
+              <th>Longitude</th>
+              <th>FileID</th>
+              <th>DateTime</th>
+              <th>Description</th>
+              <th>Long Description</th>
+              <th>Edit</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentPageData.map((item) => (
+              <tr key={item.$id} className="table-row">
+                <td>{item.Name}</td>
+                <td>{item.Latitude}</td>
+                <td>{item.Longitude}</td>
+                <td>{item.FileID}</td>
+                <td>{item.DateTime}</td>
+                <td>{item.EventListDescription}</td>
+                <td>{item.EventDetailsDescription}</td>
+
+                <td>
+                  <button  className="edit-button" onClick={() => editEvent(item)}>Edit</button>
+                </td>
+                <td>
+                  <button  className="delete-button" onClick={() => handleDeleteClick(item)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="pagination">
+        <button onClick={fetchPreviousPage} disabled={currentPage === 1}>Previous</button>
+        <span className="page">{`Page ${currentPage}`}</span>
+        <button onClick={fetchNextPage} disabled={currentPage === totalPages}>Next</button>
+      </div>
+    </div>
+  );
+};
+
+export default EventsTable;
+
+
--- src/Pages/dashboard/MainComponents/EventsEdit.jsx
+++ src/Pages/dashboard/MainComponents/EventsEdit.jsx
@@ -1,41 +1,85 @@
 // EventEdit.jsx
 
-import React, { useState } from "react";
-import CreateEvent from "../EventComponents/CreateEvent";
-import DeleteEvent from "../EventComponents/DeleteEvent";
-import EditEvent from "../EventComponents/EditEvent";
-import "../../../styling/EventsEditStyle.css";
+import React, { useState, useEffect} from "react";
+import { Query } from "appwrite";
+
+import "../../../styling/EventsStyling/EventsEditStyle.css";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import EventsTable from "../EventComponents/EventsTable";
+import {database} from "../../../utils/AppwriteConfig";
+import { EVENTS_COLLECTION_ID } from "../../../utils/AppwriteConfig";
+import { DATABASE_ID } from "../../../utils/AppwriteConfig";
+import { storage } from "../../../utils/AppwriteConfig";
+import { BUCKET_ID } from "../../../utils/AppwriteConfig";
 
-export default function EventEdit() {
-  const [remountCreateEvent, setRemountCreateEvent] = useState(false);
-  const [remountDeleteEvent, setRemountDeleteEvent] = useState(false);
-  const [remountEditEvent, setRemountEditEvent] = useState(false);
 
+
+export default function EventsEdit(){
+  const [data,setData] = useState([]);
   const navigate = useNavigate();
+
+useEffect(() => {
+getEvents();
+},[]);
+
 
   const navigateToDash = () => {
     navigate("/dash");
   };
 
-  const handleCreateEventRemounts = () => {
-    // setRemountCreateEvent((prevValue) => !prevValue);
-    setRemountDeleteEvent((prevValue) => !prevValue);
-    setRemountEditEvent((prevValue) => !prevValue);
+  const getEvents = async () => {
+    try {
+      const response = await database.listDocuments(
+        DATABASE_ID,
+        EVENTS_COLLECTION_ID,
+        [
+          Query.limit(1000), // Fetch all documents
+          Query.offset(0)
+        ]
+      );
+      setData(response.documents);
+    } catch (error) {
+      console.error("Error fetching documents:", error);
+    }
   };
 
-  const handleDeleteEventRemounts = () => {
-    setRemountDeleteEvent((prevValue) => !prevValue);
-    setRemountEditEvent((prevValue) => !prevValue);
 
+  const  deleteEvent = async (event) =>{
+        try {
+          await database.deleteDocument(DATABASE_ID, EVENTS_COLLECTION_ID, event.$id);
+          deleteImage(event);
+          getEvents();
+        } catch (error) {
+          console.error("Error deleting document:", error);
+        }
+  }; 
+
+  const deleteImage = async (event) =>{
+    const promise = storage.deleteFile(BUCKET_ID,event.FileID);
+
+    promise.then(function (response) {
+      console.log(response); // Success
+  }, function (error) {
+      console.log(error); // Failure
+  }); 
   };
 
-  const handleEditEventRemounts = () => {
-    setRemountEditEvent((prevValue) => !prevValue);
-    setRemountDeleteEvent((prevValue) => !prevValue);
-
+  
+  const editEvent = (event) => {
+    navigate("/editEvent", {
+      state: {
+        Event: event,
+      }
+    });
   };
+  const createEvent = () => {
+    navigate("/createEvent");
+  };
+
+
+
+
 
   return (
     <div>
@@ -45,10 +89,14 @@ export default function EventEdit() {
         </Button>
       </div>
       <div className="eventsEdit">
-        <CreateEvent onDataChange={handleCreateEventRemounts} />
-        <DeleteEvent onDataChange={handleDeleteEventRemounts} />
-        <EditEvent onDataChange={handleEditEventRemounts} />
+        <EventsTable 
+        data = {data}
+        deleteEvent = {deleteEvent}  
+        editEvent = {editEvent}
+        createEvent = {createEvent}
+        />
       </div>
-    </div>
+  </div>
   );
 }
+
