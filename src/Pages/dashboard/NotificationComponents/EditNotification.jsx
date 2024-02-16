46e4e232b27178eef35f2d29396123a57adc8608
mas8190 <mas8190>
2024-02-16 16:23:50 -0500

    edit notifictation logic with styling for drpwdowns

---
+++ src/Pages/dashboard/NotificationComponents/EditNotification.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../../styling/NotificationStyling/EditNotificationStyle.css";
+import {ID} from "appwrite";
+import {database} from "../../../utils/AppwriteConfig";
+
+import {ALERTS_COLLECTION_ID} from "../../../utils/AppwriteConfig";
+import {DATABASE_ID} from "../../../utils/AppwriteConfig";
+import { toast ,ToastContainer} from "react-toastify";
+import { useLocation } from "react-router-dom";
+
+
+
+export default function EditNotification(){
+    const location = useLocation();
+    let notification = location.state.notification;
+
+const [title,setTitle] = useState(notification.Title);
+const [details,setDetails] = useState(notification.Details);
+const [alertType,setAlertType] = useState(notification.AlertType);
+const [notificationType,setNotificationType] = useState(notification.NotificationType);
+
+const navigate = useNavigate();
+
+const Successfull = () => {
+    toast.success("Notification Update", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  
+  };
+  
+  const Failed = () => {
+    toast.error("Failed to Update Notification", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+  
+
+useEffect(()=>{
+
+});
+
+const handleSubmit = async () => {
+
+    console.log(alertType);
+
+   const data = {
+    Title: title,
+    Details: details,
+    AlertType: alertType,
+    NotificationType: notificationType,
+   };
+
+   try{
+    const response = await database.updateDocument(DATABASE_ID,ALERTS_COLLECTION_ID,notification.$id,data);
+    Successfull();
+   }catch(response){
+    Failed();
+    console.log(response);
+   }
+
+};
+
+
+
+    return(
+        <div>
+        <ToastContainer/>
+            <div className="editNotificationContainer">
+                <h1 className="title">Edit Notification</h1>
+                    <input type="text" value= {notification.Title} onChange={(e) => setTitle(e.target.value)}  />
+                    <input type="text" value={notification.Details} onChange={(e) => setDetails(e.target.value)} />
+
+        <div className="dropdown-container">
+            <label className="dropdown-label">
+                Alert Type:      
+                <select className="dropdown" value={alertType} onChange={(e) => setAlertType(e.target.value)}>
+                <option value="in-app">In-App</option>
+                <option value="push">Push</option>
+                <option value="both">Both</option>
+                </select>
+            </label>        
+
+            <label className="dropdown-label">
+                Notification Type:      
+                <select className="dropdown" value={notificationType} onChange={(e) => setNotificationType(e.target.value)}>
+                <option value="alerts">Alerts</option>
+                <option value="events">Events</option>
+                <option value="promos">Promos</option>
+                </select>
+            </label>  
+        </div>
+        
+        <button onClick={handleSubmit}>Edit Notification</button>
+        <button onClick={() => navigate(-1)}>go back</button>
+
+        </div>
+        </div>
+    );
+}
