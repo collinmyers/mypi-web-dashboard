086955c02b039e17d6cf38c83b4a0cb3003e146e
mas8190 <mas8190>
2024-02-27 19:30:51 -0500

    new styling for poi table using mui material

--- src/Pages/dashboard/POIComponents/POITable.jsx
+++ src/Pages/dashboard/POIComponents/POITable.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
 import "../../../styling/POIStyling/POITable.css";
+import PropTypes from "prop-types";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
+import {
+  Box,
+  Card,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  TablePagination,
+  TextField,
+  Button,
+} from "@mui/material";
+import { styled } from "@mui/system";
+
+const ScrollableTableCell = styled(TableCell)({
+  minWidth: 0,
+  maxWidth: 200,
+  width: 200,
+  whiteSpace: "nowrap",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  maxHeight: 100
+});
 
 const POITable = ({ allData, onEdit, onDelete, onCreate }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize] = useState(7);
+  const [pageSize] = useState(6);
 
   const handleSearchChange = (event) => {
     const value = event.target.value.toLowerCase();
@@ -23,66 +50,77 @@ const POITable = ({ allData, onEdit, onDelete, onCreate }) => {
     item.Name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  const totalPages = Math.ceil(filteredData.length / pageSize);
   const startIndex = (currentPage - 1) * pageSize;
   const currentPageData = filteredData.slice(startIndex, startIndex + pageSize);
 
-  const fetchPreviousPage = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
-  };
-
-  const fetchNextPage = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
-  };
-
   return (
-    <div>
-      <div className="search-container">
-        <input type="text" className = "searchBar" placeholder="Search by name..." value={searchTerm} onChange={handleSearchChange} />
-      </div>
-      <div className="table-container">
-        <div className="create-poi-container">
-          <button onClick={() => onCreate()} className="create-poi-button">Create POI</button>
-        </div>
-        <table className="custom-table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Latitude</th>
-              <th>Longitude</th>
-              <th>Status</th>
-              <th>Type</th>
-              <th>Edit</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {currentPageData.map((item) => (
-              <tr key={item.$id} className="table-row">
-                <td>{item.Name}</td>
-                <td>{item.Latitude}</td>
-                <td>{item.Longitude}</td>
-                <td>{item.Status}</td>
-                <td>{item.Type}</td>
-                <td>
-                  <button onClick={() => onEdit(item)} className="edit-button">Edit</button>
-                </td>
-                <td>
-                <button onClick={() => handleDeleteClick(item.$id)} className="btn btn-danger">
-                Delete</button>                
-              </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <div className="pagination">
-        <button onClick={fetchPreviousPage} disabled={currentPage === 1}>Previous</button>
-        <span className="page">{`Page ${currentPage}`}</span>
-        <button onClick={fetchNextPage} disabled={currentPage === totalPages}>Next</button>
-      </div>
-    </div>
+    <Card>
+      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", minWidth: 800 }}>
+        <TextField
+          label="Search by name..."
+          variant="outlined"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          sx={{ mb: 2 }}
+        />
+        <Button className="NewPOI" onClick={onCreate} startIcon={<AddIcon/>} sx={{ mb: 2, mr:3 }}>
+          New
+        </Button>
+      </Box>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>Latitude</TableCell>
+            <TableCell>Longitude</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell>Edit</TableCell>
+            <TableCell>Delete</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {currentPageData.map((item) => (
+            <TableRow key={item.$id}>
+              <ScrollableTableCell>{item.Name}</ScrollableTableCell>
+              <ScrollableTableCell>{item.Latitude}</ScrollableTableCell>
+              <ScrollableTableCell>{item.Longitude}</ScrollableTableCell>
+              <ScrollableTableCell>{item.Status}</ScrollableTableCell>
+              <ScrollableTableCell>{item.Type}</ScrollableTableCell>
+              <TableCell>
+                <Button onClick={() => onEdit(item)} startIcon={<EditIcon/>} >
+               
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button
+                  onClick={() => handleDeleteClick(item.$id)}
+                  startIcon= {<DeleteIcon/>}
+                >
+                </Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <TablePagination
+        component="div"
+        count={filteredData.length}
+        onPageChange={(event, page) => setCurrentPage(page + 1)}
+        page={currentPage - 1}
+        rowsPerPage={pageSize}
+        rowsPerPageOptions={[pageSize]}
+      />
+    </Card>
   );
+};
+
+POITable.propTypes = {
+  allData: PropTypes.array,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
+  onCreate: PropTypes.func,
 };
 
 export default POITable;
