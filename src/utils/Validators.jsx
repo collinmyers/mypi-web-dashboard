1e07875b2498817f76be6bdb88c0b8e4f8fb1f15
mas8190 <mas8190>
2024-04-02 16:31:13 -0400

    updated validators

--- src/utils/Validators.jsx
+++ src/utils/Validators.jsx
@@ -1,26 +1,26 @@
-export const validateName = (name, setName) => {
-    const nameRegex = /^[a-zA-Z]+$/;
-
-    if (!nameRegex.test(name)) {
-        setName("");
-    }
+// validators.jsx
+export const validateName = (name) => {
+    return name.length > 0;  // True if name has at least one character
 };
 
-export const validateEmail = (email, setEmail) => {
-    const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if (!emailRegex.test(email)) {
-        setEmail("");
-    }
+export const validateEmail = (email) => {
+    const emailRegex = /^[a-zA-Z0-9.!#$%&’+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)$/;
+    return emailRegex.test(email);
 };
 
-export const validatePassword = (password, confirmPassword, setPassword, setConfirmPassword) => {
+export const validatePassword = (password, confirmPassword) => {
+
+    let passwordErrors = [];
+
     if (password.length < 8) {
-        setPassword("");
+        passwordErrors.push("Password must be at least 8 characters");
     }
 
     if (password !== confirmPassword) {
-        setPassword("");
-        setConfirmPassword("");
+        passwordErrors.push("Passwords do not match");
     }
+
+    if (passwordErrors.length > 0) return passwordErrors;
+
+    return ""; // Return an empty string if validation passes
 };
